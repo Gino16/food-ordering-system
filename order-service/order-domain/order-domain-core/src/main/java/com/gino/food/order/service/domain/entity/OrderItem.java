df9d73057fbeeb115026a91eed259c4fcc6c2da0
Gino Ascencio <gino.francisco@hotmail.com>
2023-05-08 22:11:40 -0500

    added exception

--- order-service/order-domain/order-domain-core/src/main/java/com/gino/food/order/service/domain/entity/OrderItem.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/gino/food/order/service/domain/entity/OrderItem.java
@@ -7,11 +7,11 @@ import com.gino.food.order.service.domain.valueobject.OrderItemId;
 
 public class OrderItem extends AggregateRoot<OrderItemId> {
 
-  private OrderId orderId;
   private final Product product;
   private final int quantity;
   private final Money price;
   private final Money subTotal;
+  private OrderId orderId;
 
   private OrderItem(Builder builder) {
     super.setId(builder.id);
@@ -21,6 +21,15 @@ public class OrderItem extends AggregateRoot<OrderItemId> {
     subTotal = builder.subTotal;
   }
 
+  void initializeOrderItem(OrderId orderId, OrderItemId orderItemId) {
+    this.orderId = orderId;
+    super.setId(orderItemId);
+  }
+
+  boolean isPriceValid() {
+    return price.isGreaterThanZero() && price.equals(product.getPrice()) && price.multiply(quantity)
+        .equals(subTotal);
+  }
 
   public OrderId getOrderId() {
     return orderId;
---
+++ order-service/order-domain/order-domain-core/src/main/java/com/gino/food/order/service/domain/exception/OrderDomainException.java
@@ -0,0 +1,14 @@
+package com.gino.food.order.service.domain.exception;
+
+import com.gino.food.domain.exception.DomainException;
+
+public class OrderDomainException extends DomainException {
+
+  public OrderDomainException(String message) {
+    super(message);
+  }
+
+  public OrderDomainException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
