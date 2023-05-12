d34aa2c8181428b1b46573146886a6e48ac060b5
Gino <gino.francisco@hotmail.com>
2023-05-11 22:02:34 -0500

    feat: added state changing in Order

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/gino/food/order/service/domain/entity/Customer.java
@@ -0,0 +1,8 @@
+package com.gino.food.order.service.domain.entity;
+
+import com.gino.food.domain.entity.AggregateRoot;
+import com.gino.food.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+
+}
