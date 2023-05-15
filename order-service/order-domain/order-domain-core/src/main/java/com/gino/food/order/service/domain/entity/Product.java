b00118a5f6e22eb4ec099b6642cd0aafcd7526ea
Gino Ascencio <gino.francisco@hotmail.com>
2023-05-15 16:49:32 -0500

    finish order domain core

--- order-service/order-domain/order-domain-core/src/main/java/com/gino/food/order/service/domain/entity/Product.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/gino/food/order/service/domain/entity/Product.java
@@ -5,11 +5,17 @@ import com.gino.food.domain.valueobject.Money;
 import com.gino.food.domain.valueobject.ProductId;
 
 public class Product extends BaseEntity<ProductId> {
+
   private String name;
   private Money price;
 
   public Product(ProductId productId, String name, Money price) {
     super.setId(productId);
+    this.name = name;
+    this.price = price;
+  }
+
+  public void updateWithConfirmedNameAndPrice(String name, Money price) {
     this.name = name;
     this.price = price;
   }
