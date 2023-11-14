5d41a7135182cbde56f7ef8efac653309fbc1c0b
Gino Ascencio <gino.francisco@hotmail.com>
2023-11-13 20:11:42 -0500

    added order entity implementation

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
@@ -41,6 +41,17 @@ public class OrderItem extends BaseEntity<OrderItemId> {
     return subTotal;
   }
 
+  public void initializeOrderItem(OrderId orderId, OrderItemId orderItemId) {
+    this.orderId = orderId;
+    super.setId(orderItemId);
+  }
+
+  boolean isPriceValid() {
+    return price.isGreaterThanZero() &&
+        price.equals(product.getPrice()) &&
+        price.multiply(quantity).equals(subTotal);
+  }
+
   public static final class Builder {
 
     private OrderItemId orderItemId;
