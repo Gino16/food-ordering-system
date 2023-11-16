df5d79c36ab87f96c3d152b441979d823ac8f5f6
Gino Ascencio <gino.francisco@hotmail.com>
2023-11-16 08:41:43 -0500

    implemented order track command handler

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/servide/domain/OrderTrackCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/servide/domain/OrderTrackCommandHandler.java
@@ -1,15 +1,33 @@
 package com.food.ordering.system.order.servide.domain;
 
+import com.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
+import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
 import com.food.ordering.system.order.servide.domain.dto.track.TrackOrderQuery;
 import com.food.ordering.system.order.servide.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.order.servide.domain.mapper.OrderDataMapper;
+import com.food.ordering.system.order.servide.domain.ports.output.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class OrderTrackCommandHandler {
 
-  public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+  private final OrderDataMapper orderDataMapper;
+  private final OrderRepository orderRepository;
 
+  @Transactional(readOnly = true)
+  public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+    var orderResult =
+        orderRepository.findByTrackingId(new TrackingId(trackOrderQuery.getOrderTrackingId()));
+    if (orderResult.isEmpty()) {
+      log.warn("Could not find order with tracking id: {}", trackOrderQuery.getOrderTrackingId());
+      throw new OrderNotFoundException(
+          "Could not find order with tracking id:" + trackOrderQuery.getOrderTrackingId());
+    }
+    return orderDataMapper.orderToTrackOrderResponse(orderResult.get());
   }
 }
