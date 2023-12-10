5e2918b0776b14044fea7916248280c2acdc92d6
Gino Ascencio <gino.francisco@hotmail.com>
2023-12-10 16:42:18 -0500

    finish order approval and order payment saga refactor to use outbox

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/outbox/model/approval/OrderApprovalOutboxMessage.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/outbox/model/approval/OrderApprovalOutboxMessage.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Builder
@@ -17,25 +18,16 @@ public class OrderApprovalOutboxMessage {
   private UUID id;
   private UUID sagaId;
   private ZonedDateTime createdAt;
+  @Setter
   private ZonedDateTime processedAt;
   private String type;
   private String payload;
+  @Setter
   private SagaStatus sagaStatus;
+  @Setter
   private OrderStatus orderStatus;
   private OutboxStatus outboxStatus;
   private int version;
-
-  public void setProcessedAt(ZonedDateTime processedAt) {
-    this.processedAt = processedAt;
-  }
-
-  public void setSagaStatus(SagaStatus sagaStatus) {
-    this.sagaStatus = sagaStatus;
-  }
-
-  public void setOrderStatus(OrderStatus orderStatus) {
-    this.orderStatus = orderStatus;
-  }
 
   public void setOutboxStatus(OutboxStatus outboxStatus) {
     this.outboxStatus = outboxStatus;
